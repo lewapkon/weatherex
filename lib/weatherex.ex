d8305bfdf0c261940eebae5af4645605f879d2e3
Paweł Koniarski <me@pkoniarski.com>
2017-05-16 22:32:00 +0200

    Init project

---
+++ lib/weatherex.ex
@@ -0,0 +1,18 @@
+defmodule Weatherex do
+  @moduledoc """
+  Documentation for Weatherex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Weatherex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/weatherex_test.exs
@@ -0,0 +1,8 @@
+defmodule WeatherexTest do
+  use ExUnit.Case
+  doctest Weatherex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
